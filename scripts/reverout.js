e7ae9a7f883a42c6fbcd176bfd5f42340951bac6
DeltaNedas <deltanedas@gmail.com>
2020-06-13 03:43:24 +0100

    1.10.2: fix reverout ro(u)ters

--- scripts/reverout.js
+++ scripts/reverout.js
@@ -9,47 +9,26 @@ const rotors = [
 	-1, 1
 ];
 
-const drone = extendContent(UnitType, "reverout", {
+const reverout = extendContent(UnitType, "reverout", {
 	load() {
 		this.weapon.region = Core.atlas.find("clear");
 		this.region = Core.atlas.find(this.name);
 		this.rotor = Core.atlas.find("router");
-	},
-
-	draw(unit) {
-		const r = unit.rotation;
+	}
+});
+reverout.constructor = prov(() => extend(FlyingUnit, {
+	drawOver() {
+		const r = this.rotation;
 		const sin = Mathf.sin(r) * dist;
 		const cos = Mathf.cos(r) * dist;
 		var x, y;
 		for (var i = 0; i < 8; i += 2) {
 			x = rotors[i] * offset;
 			y = rotors[i + 1] * offset;
-			Draw.rect(this.rotor,
-				unit.x + Angles.trnsx(r, x, y),
-				unit.y + Angles.trnsy(r, x, y),
+			Draw.rect(reverout.rotor,
+				this.x + Angles.trnsx(r, x, y),
+				this.y + Angles.trnsy(r, x, y),
 				r + Time.time() * 20);
 		}
-	},
-
-	drawStats(unit) {
-		if (this.drawCell){
-			const health = unit.healthf();
-			const rot = unit.rotation - 90;
-			Draw.color(Color.black, unit.team.color, health + Mathf.absin(Time.time(), Math.max(health * 5, 1), 1 - health));
-			Draw.rect(unit.getPowerCellRegion(),
-				unit.x + Angles.trnsx(rot, 0, -5.5),
-				unit.y + Angles.trnsy(rot, 0, -5.5),
-				rot);
-			Draw.color();
-		}
-
-		if (this.drawItems) {
-			unit.drawBackItems();
-		}
-
-		if (this.lightEmitted > 0) {
-			unit.drawLight(this.lightEmitted);
-		}
 	}
-});
-drone.constructor = prov(() => new FlyingUnit());
+}));
