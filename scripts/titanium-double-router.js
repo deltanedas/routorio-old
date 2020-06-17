9eddcaee38ed558ab899063cfcfc7232647b259c
DeltaNedas <deltanedas@gmail.com>
2020-06-17 13:25:33 +0100

    refactor

--- scripts/titanium-double-router.js
+++ scripts/titanium-double-router.js
@@ -18,27 +18,24 @@ const titaniumDoubleRouter = extendContent(Router, "titanium-double-router", {
 		return [Core.atlas.find(this.name)];
 	},
 
-	calcOffset(tile) {
-		var x = tile.x;
-		return x + ((x % 2) ? -1 : 1);
-	},
+	calcOffset: x => x + ((x % 2) ? -1 : 1),
 
 	canPlaceOn(tile){
-		const x = this.calcOffset(tile);
+		const x = this.calcOffset(tile.x);
 		const other = Vars.world.tile(x, tile.y);
 		return other.block() == "air"
 	},
 
 	placed(tile) {
 		this.super$placed(tile);
-		const x = this.calcOffset(tile);
+		const x = this.calcOffset(tile.x);
 		Call.setTile(Vars.world.tile(x, tile.y), this, tile.team, 0);
 	},
 
 	removed(tile) {
 		this.super$removed(tile);
-		const x = this.calcOffset(tile);
-		const key = tile.x + "," + tile.y;
+		const x = this.calcOffset(tile.x);
+
 		/* Prevent trying to delete the other half infinitely */
 		if (!lock) {
 			lock = true;
