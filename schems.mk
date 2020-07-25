e28ef0938c2ecf99bd131a314e1f035158744b0a
DeltaNedas <deltanedas@gmail.com>
2020-07-25 19:59:56 +0100

    add router chainer to schems

--- schems.mk
+++ schems.mk
@@ -14,7 +14,7 @@ power := arc-router electric-router moderouter solar-router surge-router
 prod := ubuntium-router
 
 blocks := $(dist:%=distribution/%) $(power:%=power/%) $(prod:%=production/%) \
-	units/reverout-factory units/router-house
+	units/reverout-factory units/router-house units/router-chainer-icon
 
 units := reverout routerpede
 
