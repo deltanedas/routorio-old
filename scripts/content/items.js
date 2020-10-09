760439303c3647048e873c97213089eb35437f3f
DeltaNedas <deltanedas@gmail.com>
2020-10-09 20:51:23 +0100

    2.30.1: anuke II

--- scripts/content/items.js
+++ scripts/content/items.js
@@ -1,19 +1,5 @@
 const routorio = this.global.routorio;
 
-const beryllium = extendContent(Item, "beryllium", {
-	init() {
-		this.super$init();
-		routorio.research(this, "ubuntium-router");
-	},
-
-	researchRequirements: () => ItemStack.with(
-		Items.silicon, 3000,
-		Items.graphite, 7000,
-		Items.titanium, 4500),
-
-	color: Color.valueOf("#c3d6c7")
-});
-
 module.exports = {
-	beryllium: beryllium
+	beryllium: new Item("beryllium")
 };
